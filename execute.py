375cfb4f85ff9c82a857f353ed4e62afaed4432c
doriszhang <doriszhangch@gmail.com>
2022-02-09 10:55:24 +0100

    argparse in execute

--- execute.py
+++ execute.py
@@ -6,43 +6,56 @@ from dataloader import cocoData, collate_fn
 from model import Encoder, Decoder
 from train import *
 from voc import build_voc
+import argparse
 
 if __name__ == '__main__':
 
-    data = []
-    with open('parameters.txt', 'r') as f:
-        for line in f:
-            data.append(str(line).strip('\n').split(' ')[1])
-    f.close()
+    parser = argparse.ArgumentParser(
+        description="Some description."
+    )
 
-    # print(data)
-    train_info = data[0]
-    train_image = data[1]
-    nmin = int(data[2])
-    batch_size = int(data[3])
-    shuffle = bool(data[4])
-    num_workers = int(data[5])
-    embed_size = int(data[6])
-    hidden_size = int(data[7])
-    max_length = int(data[8])
-    lr = float(data[9])
+    parser.add_argument("--train_info", type=str,
+                        default="/Users/doriszhang/coco2017/annotations/captions_val2017.json")
+    parser.add_argument("--train_image", type=str,
+                        default="/Users/doriszhang/coco2017/val2017")
+    parser.add_argument("--nmin", type=int,
+                        default=50)
+    parser.add_argument("--batch_size", type=int,
+                        default=128)
+    parser.add_argument(
+        "--deterministic", action="store_false", help="Whether to shuffle the data. Default is True.",
+    )
+    parser.add_argument("--num_workers", type=int,
+                        default=2)
+    parser.add_argument("--embed_size", type=int,
+                        default=256)
+    parser.add_argument("--hidden_size", type=int,
+                        default=512)
+    parser.add_argument("--max_length", type=int,
+                        default=15)
+    parser.add_argument("--lr", type=float,
+                        default=0.001)
 
+    parser.add_argument("--encoder_save_path", type=str,
+                        default="/mnt/vol_b/encoder.pth")
+    parser.add_argument("--decoder_save_path", type=str,
+                        default="/mnt/vol_b/decoder.pth")
 
-    coco = COCO(train_info)
+    args = parser.parse_args()
 
-    voc = build_voc(coco, nmin)
-    dataset = cocoData(coco, train_image, voc, transform = transforms.Compose([transforms.Resize((256,256)), transforms.ToTensor()]))
-    train_data = torch.utils.data.DataLoader(dataset, batch_size=batch_size ,shuffle=shuffle , num_workers=num_workers , collate_fn=collate_fn)
+    coco = COCO(args.train_info)
+
+    voc = build_voc(coco, args.nmin)
+    dataset = cocoData(coco, args.train_image, voc, transform = transforms.Compose([transforms.Resize((256,256)), transforms.ToTensor()]))
+    train_data = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size ,shuffle=args.deterministic , num_workers=args.num_workers , collate_fn=collate_fn)
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    encoder = Encoder(embed_size=embed_size).to(device)
-    decoder = Decoder(embed_size=embed_size, hidden_size=hidden_size, voc_size=len(voc), max_length=max_length).to(device);
+    encoder = Encoder(embed_size=args.embed_size).to(device)
+    decoder = Decoder(embed_size=args.embed_size, hidden_size=args.hidden_size, voc_size=len(voc), max_length=args.max_length).to(device)
 
-    train(encoder, decoder, train_data, device, lr)
-    PATH1 = '/mnt/vol_b/encoder.pth'
-    PATH2 = '/mnt/vol_b/decoder.pth'
-    torch.save(encoder.state_dict(), PATH1, _use_new_zipfile_serialization = False)
-    torch.save(decoder.state_dict(), PATH2, _use_new_zipfile_serialization = False)
+    train(encoder, decoder, train_data, device, args.lr)
+    torch.save(encoder.state_dict(), args.encoder_save_path, _use_new_zipfile_serialization = False)
+    torch.save(decoder.state_dict(), args.decoder_save_path, _use_new_zipfile_serialization = False)
 
 
 
